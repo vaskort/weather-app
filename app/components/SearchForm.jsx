b2dca0b2581abe0fd2783cd15c10c0cb1f5ff373
Vasilis Kortsimelidis <vaskort@gmail.com>
2017-07-22 14:24:39 +0100

    Dispatched action from searchForm

--- app/components/SearchForm.jsx
+++ app/components/SearchForm.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { getData } from 'weatherActions';
+import { getData, setFetching } from 'weatherActions';
 
 class SearchForm extends Component {
   handleForm = (e) => {
     e.preventDefault();
-    this.props.getData('London');
+    this.props.setFetching(true);
+    this.props.getData(this.refs.city.value)
+      .then(resp => {
+        this.props.setFetching(false);
+      })
+      .catch(err => {
+        console.log(err);
+        this.props.setFetching(false);
+      });
   }
 
   render() {
@@ -15,7 +23,7 @@ class SearchForm extends Component {
         <form onSubmit={this.handleForm}>
           <div className="grid-x grid-margin-x">
             <div className="small-12 medium-8 large-4 large-offset-4 cell">
-              <input type="text" placeholder="Name of city"/>
+              <input type="text" placeholder="Name of city" ref="city" value="London"/>
             </div>
             <div className="small-12 medium-4 large-4 large-offset-4 cell">
               <button type="submit" className="button expanded">Search</button>
@@ -36,7 +44,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     getData: (city) => {
-      dispatch(getData(city));
+      return dispatch(getData(city));
+    },
+    setFetching: (payload) => {
+      dispatch(setFetching(payload));
     }
   }
 }
