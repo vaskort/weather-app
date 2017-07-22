2fc7ca60f0de8dcd6cbf97be4b63668a3a413b4f
Vasilis Kortsimelidis <vaskort@gmail.com>
2017-07-22 14:23:54 +0100

    Created two new Actions SET_FETCHING, GET_DATA_FULFILLED

--- app/actions/weatherActions.js
+++ app/actions/weatherActions.js
@@ -1,6 +1,19 @@
+import axios from 'axios';
+
 export function getData(city) {
+
+  const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=b12b50103893779b9ed06c4e9346cec9&units=metric';
+  let requestURL = `${OPEN_WEATHER_MAP_URL}&q=${city},GB`;
+
   return {
     type: "GET_DATA",
-    payload: city
+    payload: axios.get(requestURL)
+  };
+}
+
+export function setFetching(payload) {
+  return {
+    type: "SET_FETCHING",
+    payload: payload
   };
 }
--- app/reducers/weatherReducer.js
+++ app/reducers/weatherReducer.js
@@ -1,10 +1,15 @@
 import { Map } from 'immutable';
 
 const weatherReducer = (state = Map({
+  fetching: false,
   weatherData:[]
 }), action) => {
   switch (action.type) {
-    case "GET_DATA":
+    case "SET_FETCHING":
+      state = state.merge({fetching: action.payload});
+      console.log('hi');
+      break;
+    case "GET_DATA_FULFILLED":
       break;
   }
   return state;
